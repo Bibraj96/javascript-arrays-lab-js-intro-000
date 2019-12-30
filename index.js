64b099b70261534c099f38955c31aa39bf756524
Blendi Ibraj <blendi.ibraj96@gmail.com>
2019-12-30 23:46:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,3 @@ function destructivelyRemoveLastKitten() {
 function destructivelyRemoveFirstKitten(){
     return kittens.shift()
 }
-
-function appendKitten(name) {
-  return kittens [...kittens, name]
-}
